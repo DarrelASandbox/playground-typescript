87742f51980da57b42e0ab7156ceed1cb719ae0c
DarrelA <darrelaiscoding@gmail.com>
2022-12-30 23:30:57 +0800

    Filtering projects with enums

--- maximilian_schwarzmuller/07_drag_and_drop_project/src/app.ts
+++ maximilian_schwarzmuller/07_drag_and_drop_project/src/app.ts
@@ -115,7 +115,12 @@ class ProjectList {
     this.element.id = `${this.type}-projects`;
 
     projectState.addListener((projects: Project[]) => {
-      this.assignedProjects = projects;
+      const relevantProjects = projects.filter((prj) => {
+        if (this.type === 'active') return prj.status === ProjectStatus.Active;
+        return prj.status === ProjectStatus.Finished;
+      });
+
+      this.assignedProjects = relevantProjects;
       this.renderProjects();
     });
 
@@ -137,6 +142,9 @@ class ProjectList {
     const listEl = document.getElementById(
       `${this.type}-projects-list`
     )! as HTMLUListElement;
+
+    listEl.innerHTML = ''; // Clear and rerender
+
     for (const prjItem of this.assignedProjects) {
       const listItem = document.createElement('li');
       listItem.textContent = prjItem.title;
