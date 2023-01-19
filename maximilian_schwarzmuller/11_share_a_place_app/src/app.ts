70df18a219b31499f2699bd52ead2510c8600ce3
DarrelA <darrelaiscoding@gmail.com>
2023-01-19 23:10:53 +0800

    `axios` package

--- maximilian_schwarzmuller/11_share_a_place_app/src/app.ts
+++ maximilian_schwarzmuller/11_share_a_place_app/src/app.ts
@@ -1,4 +1,11 @@
+import axios from 'axios';
+
 const GOOGLE_API_KEY = '';
+
+type GoogleGeocodingResponse = {
+  results: { geometry: { location: { lat: number; lng: number } } }[];
+  status: 'OK' | 'ZERO_RESULTS';
+};
 
 const form = document.querySelector('form');
 const addressInput = document.getElementById('address') as HTMLInputElement;
@@ -6,6 +13,20 @@ const addressInput = document.getElementById('address') as HTMLInputElement;
 function searchAddressHandler(e: Event) {
   e.preventDefault();
   const enteredAddress = addressInput.value;
+
+  axios
+    .get<GoogleGeocodingResponse>(
+      `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(
+        enteredAddress
+      )}&key=${GOOGLE_API_KEY}
+  `
+    )
+    .then((res) => {
+      if (res.data.status !== 'OK') throw new Error('Could not fetch location!');
+      const coordinates = res.data.results[0].geometry.location;
+      console.log(coordinates);
+    })
+    .catch((err) => console.log(err));
 }
 
 form?.addEventListener('submit', searchAddressHandler);
