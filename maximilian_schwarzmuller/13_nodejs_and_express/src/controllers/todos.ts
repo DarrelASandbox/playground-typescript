4341bc4c6a60d34abacf483220473ead72731e98
DarrelA <darrelaiscoding@gmail.com>
2023-01-23 19:34:14 +0800

    Routes: `getTodos`, `updateTodo` & `deleteTodo`

--- maximilian_schwarzmuller/13_nodejs_and_express/src/controllers/todos.ts
+++ maximilian_schwarzmuller/13_nodejs_and_express/src/controllers/todos.ts
@@ -13,4 +13,27 @@ const createTodo: RequestHandler = (req, res, next) => {
   res.status(201).json({ message: 'Created the todo.', createdTodo: newTodo });
 };
 
-export { createTodo };
+const getTodos: RequestHandler = (req, res, next) => {
+  res.json({ todos: TODOS });
+};
+
+const updateTodo: RequestHandler<{ id: string }> = (req, res, next) => {
+  const todoId = req.params.id;
+  const updatedText = (req.body as { text: string }).text; // Type casting
+  const todoIndex = TODOS.findIndex((todo) => todo.id === todoId);
+
+  if (todoIndex < 0) throw new Error('Could not find todo!');
+  TODOS[todoIndex] = new Todo(TODOS[todoIndex].id, updatedText);
+  res.json({ message: 'Updated!', updateTodo: TODOS[todoIndex] });
+};
+
+const deleteTodo: RequestHandler<{ id: string }> = (req, res, next) => {
+  const todoId = req.params.id;
+  const todoIndex = TODOS.findIndex((todo) => todo.id === todoId);
+
+  if (todoIndex < 0) throw new Error('Could not find todo!');
+  TODOS.splice(todoIndex, 1);
+  res.json({ message: 'Deleted!' });
+};
+
+export { createTodo, getTodos, updateTodo, deleteTodo };
