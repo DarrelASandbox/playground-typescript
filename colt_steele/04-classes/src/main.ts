1ac1f9d4d017dbe0c689505e1ae029ebd347eca7
DarrelA <darrelaiscoding@gmail.com>
2023-05-08 00:46:47 +0800

    Working with classes

--- colt_steele/04-classes/src/main.ts
+++ colt_steele/04-classes/src/main.ts
@@ -1,6 +1,9 @@
+// annotating classes
 class Player {
   first: string;
   last: string;
+  private score: number = 0; // class field
+  readonly readme: string = 'This a new player.';
 
   constructor(first: string, last: string) {
     this.first = first;
