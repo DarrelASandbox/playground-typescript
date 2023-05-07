5d9388560e8fd42288578b90f8eb8cd8763223d6
DarrelA <darrelaiscoding@gmail.com>
2023-05-08 01:02:17 +0800

    Getter and setter

--- colt_steele/04-classes/src/main.ts
+++ colt_steele/04-classes/src/main.ts
@@ -3,9 +3,25 @@ class Player {
   constructor(
     public first: string,
     public last: string,
-    private score: number = 0,
+    private _score: number = 0,
     readonly readme: string = 'This a new player.'
   ) {}
+
+  get fullName(): string {
+    return `${this.first} ${this.last}`;
+  }
+
+  // use underscore so there is no conflict with getter `score`
+  get score(): number {
+    return this._score;
+  }
+
+  set score(newScore: number) {
+    if (newScore < 0) throw new Error('Score cannot be negative.');
+    this._score = newScore;
+  }
 }
 
 const elton = new Player('Elton', 'Steele');
+console.log(elton.fullName);
+console.log(elton.score);
