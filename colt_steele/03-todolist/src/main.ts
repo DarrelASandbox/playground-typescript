565c4fd5e805e412e06366ef4ef3e8331e98bc30
DarrelA <darrelaiscoding@gmail.com>
2023-05-07 22:31:20 +0800

    `interface Todo`

--- colt_steele/03-todolist/src/main.ts
+++ colt_steele/03-todolist/src/main.ts
@@ -1,3 +1,10 @@
+interface Todo {
+  text: string;
+  completed: boolean;
+}
+
+const todos: Todo[] = [];
+
 // Type assertions with DOM
 const btn = document.getElementById('btn') as HTMLButtonElement;
 const input = document.getElementById('todoinput') as HTMLInputElement;
@@ -6,16 +13,21 @@ const list = document.getElementById('todolist') as HTMLUListElement;
 
 const submitHandler = (e: SubmitEvent) => {
   e.preventDefault();
-  const newTodoText = input.value;
+  const newTodo: Todo = { text: input.value, completed: false };
+  createTodo(newTodo);
+  todos.push(newTodo);
+  input.value = '';
+
+  console.log(todos);
+};
+
+const createTodo = (todo: Todo) => {
   const newListItem = document.createElement('li');
   const checkbox = document.createElement('input');
-
   checkbox.type = 'checkbox';
-  newListItem.append(newTodoText);
+  newListItem.append(todo.text);
   newListItem.append(checkbox);
   list.append(newListItem);
-
-  input.value = '';
 };
 
 form.addEventListener('submit', submitHandler);
