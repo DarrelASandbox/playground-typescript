20965eafc1a2a206a43f943226d227e12b2a214d
DarrelA <darrelaiscoding@gmail.com>
2023-05-07 22:19:41 +0800

    Building the Todo list

--- colt_steele/03-todolist/src/main.ts
+++ colt_steele/03-todolist/src/main.ts
@@ -2,15 +2,20 @@
 const btn = document.getElementById('btn') as HTMLButtonElement;
 const input = document.getElementById('todoinput') as HTMLInputElement;
 const form = document.getElementById('todoform') as HTMLFormElement;
+const list = document.getElementById('todolist') as HTMLUListElement;
 
 const submitHandler = (e: SubmitEvent) => {
   e.preventDefault();
-  console.log('first');
+  const newTodoText = input.value;
+  const newListItem = document.createElement('li');
+  const checkbox = document.createElement('input');
+
+  checkbox.type = 'checkbox';
+  newListItem.append(newTodoText);
+  newListItem.append(checkbox);
+  list.append(newListItem);
+
+  input.value = '';
 };
 
 form.addEventListener('submit', submitHandler);
-
-// btn?.addEventListener('click', () => {
-//   alert(input.value);
-//   input.value = '';
-// });
