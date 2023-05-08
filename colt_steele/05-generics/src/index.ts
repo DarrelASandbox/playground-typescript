a71d92d5a3bf9bc07f49675912386db3e842ef6b
DarrelA <darrelaiscoding@gmail.com>
2023-05-09 00:08:03 +0800

    More on generics

--- colt_steele/05-generics/src/index.ts
+++ colt_steele/05-generics/src/index.ts
@@ -32,3 +32,56 @@ console.log(getRandomElement<number>([1, 2, 3, 4, 5, 6, 7, 8, 9]));
 getRandomElement([true, false, false, true, false, true]); // Inferred generic type parameter
 
 // **********************************************
+
+// Generic with multiple types
+const merge1 = <T, U>(object1: T, object2: U) => ({ ...object1, ...object2 });
+const comboObj1 = merge1({ name: 'colt' }, { favNums: [1, 2, 3, 4, 5, 6] });
+
+// **********************************************
+
+// Adding type constraint
+const merge2 = <T extends object, U extends object>(object1: T, object2: U) => ({
+  ...object1,
+  ...object2,
+});
+
+const comboObj2 = merge2({ name: 'colt' }, { favNums: [1, 2, 3, 4, 5, 6] });
+
+// Using type interface
+interface Lengthy {
+  length: number;
+}
+
+const printDoubleLength = <T extends Lengthy>(thing: T): number => thing.length * 2;
+
+// **********************************************
+
+// Without default type parameter, strings type will be `unknown`
+const makeEmptyArray = <T = number>(): T[] => [];
+const strings = makeEmptyArray();
+
+// **********************************************
+
+// Generic classes
+interface Song {
+  title: string;
+  artist: string;
+}
+
+interface Video {
+  title: string;
+  creator: string;
+  resolution: string;
+}
+
+class Playlist<T> {
+  public queue: T[] = [];
+  add(el: T) {
+    this.queue.push(el);
+  }
+}
+
+const songs = new Playlist<Song>();
+const videos = new Playlist<Video>();
+
+// **********************************************
