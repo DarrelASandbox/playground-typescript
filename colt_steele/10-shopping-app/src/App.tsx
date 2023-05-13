0084eecc7ebdf7236c5dba3b5d39e60254a41af5
DarrelA <darrelaiscoding@gmail.com>
2023-05-13 19:47:41 +0800

    `ShoppingList` component

--- colt_steele/10-shopping-app/src/App.tsx
+++ colt_steele/10-shopping-app/src/App.tsx
@@ -1,11 +1,14 @@
-import { Greeter } from './components/Greeter';
+import { ShoppingList } from './components/ShoppingList';
 
 function App() {
+  const items = [
+    { id: 1, product: 'Lemon', quantity: 3 },
+    { id: 2, product: 'Chicken Breast', quantity: 3 },
+  ];
+
   return (
     <>
-      <Greeter person="Colt" />
-      <Greeter person="Blue" />
-      <Greeter person="Elton" />
+      <ShoppingList items={items} />
     </>
   );
 }
--- colt_steele/10-shopping-app/src/components/Greeter.tsx
+++
@@ -1,8 +0,0 @@
-interface GreeterProps {
-  person: string;
-}
-
-// destructure from `props`
-const Greeter = ({ person }: GreeterProps): JSX.Element => <h1>{`Hello, ${person}!`}</h1>;
-
-export { Greeter };
