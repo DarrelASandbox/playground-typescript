d570a60a4f4c6c8b5568fc1df981199ed5e04689
DarrelA <darrelaiscoding@gmail.com>
2023-05-13 17:16:03 +0800

    `webpack-dev-server`

--- colt_steele/09-webpack/webpack.config.js
+++ colt_steele/09-webpack/webpack.config.js
@@ -1,9 +1,14 @@
 const path = require('path');
 
 module.exports = {
+  mode: 'development',
   entry: './src/index.ts',
   devtool: 'inline-source-map',
   module: { rules: [{ test: /\.tsx?$/, use: 'ts-loader', exclude: /node_modules/ }] },
   resolve: { extensions: ['.tsx', '.ts', '.js'] },
-  output: { filename: 'bundle.js', path: path.resolve(__dirname, 'dist') },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/dist',
+  },
 };
